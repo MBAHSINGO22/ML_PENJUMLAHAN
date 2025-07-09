6b161c2a097ac04e5a580935c8b0df91713dac16
Eugenius Kriswinar Adi Cahya <140719881+MBAHSINGO22@users.noreply.github.com>
2025-07-09 15:48:34 +0700

    Add files via upload

---
+++ coba.py
@@ -0,0 +1,11 @@
+num1 = input(
+    "Masukkan angka 1: "
+)  # menggunakan scanner untuk meninput data yang dilakukan oleh user
+num2 = input(
+    "Masukkan angka 2: "
+)  # menggunakan scanner untuk meninput data yang dilakukan oleh user
+sum = num1 + num2  # melakukan proses penjumlahan dari
+
+print(
+    "Jumlah kedua angka tersebut: ", sum
+)  # mencetak hasil dari penjumlahan yang dilakukan oleh program
